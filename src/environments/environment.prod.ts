9b29b472c40ccfb4b07a1b07c7d6282c42df359e
saroj <saroj@accessibleict.com>
2018-05-01 15:37:03 +0545

    make it better

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAE0nMHDigFBU8eFOKtiYp4SvlnXlc_dzA",
+    authDomain: "accessibledoctor-c6dfc.firebaseapp.com",
+    databaseURL: "https://accessibledoctor-c6dfc.firebaseio.com",
+    projectId: "accessibledoctor-c6dfc",
+    storageBucket: "accessibledoctor-c6dfc.appspot.com",
+    messagingSenderId: "918920709032"
+  }
 };
